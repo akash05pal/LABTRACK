54aea204b2adbff18000822ed0b519e3442c9bb0
Akash pal <skypal831@gmail.com>
2025-07-31 20:34:30 +0000

    FirebaseError: Firebase: Error (auth/configuration-not-found).
    src/hook

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB5xyxQ5twmeTqv0fblWIIogNRryyNdLkQ",
+  authDomain: "labtrack-ckpvl.firebaseapp.com",
+  projectId: "labtrack-ckpvl",
+  storageBucket: "labtrack-ckpvl.firebasestorage.app",
+  messagingSenderId: "264231788271",
+  appId: "1:264231788271:web:72ced018e71a5619a3851a",
 };
 
 // Initialize Firebase
